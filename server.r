b6fb8edd89a0d8709d4db92ede17c44dc1f1aa34
Sophia Rollo <rollos24@mail.wlu.edu>
2023-11-06 16:31:47 -0500

    trying stuff

--- server.r
+++ server.r
@@ -17,11 +17,15 @@ merged_data <- left_join(bb_data, college_geo, by = "TEAM")
 
 function(input, output) {
   # Tab 1: Conferences
-  output$map <- renderLeaflet ({
-    leaflet(data = merged_data) %>% setView(lng = -95.71, lat = 37.09, zoom =3) %>% 
-      addTiles() %>%
-      addCircleMarkers(radius = 1, label = ~TEAM)
-  })
+      output$plot <- renderPlot({
+        
+        merged_data %>%
+          group_by(CONF) %>%
+          summarize(AvgFTR = mean(FTR)) %>%
+          arrange(AvgFTR) %>%
+          mutate(CONF = factor(CONF, levels = CONF, ordered = TRUE)) %>%
+          ggplot(aes(CONF, AvgFTR)) + geom_bar(stat = 'identity') + theme(axis.text.x = element_text(angle = 60, hjust = 1))
+      })
 }
 
   
