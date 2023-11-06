603b7ccae51fbe7a47ffbd80d5294199be8ad82c
Katelyn Gamble <gamblek24@mail.wlu.edu>
2023-11-06 15:22:24 -0500

    hey yall

--- server.r
+++ server.r
@@ -2,3 +2,12 @@ function(input,output) {
   
 }
   
+geodf <- read.csv("geodata.csv")
+colnames(geodf)[2] <- "TEAM"
+write.csv(geodf, "geodata.csv", row.names=FALSE)
+
+college_geo <- read.csv("geodata.csv")
+bb_data <- read.csv("cbb.csv")
+
+library(dplyr)
+merged_data <- left_join(bb_data, college_geo, by = "TEAM")
