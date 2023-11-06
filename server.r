282d1041dbb1be3f0e1883139d3d4fb49c03e5c7
Katelyn Gamble <gamblek24@mail.wlu.edu>
2023-11-06 15:42:35 -0500

    STARTING FRESH ON SERVER

--- server.r
+++ server.r
@@ -1,32 +1,33 @@
-function(input,output) {
+server <- function(input,output,session
+                )
+{
+  
 
-  geodf <- read.csv("geodata.csv")
-  colnames(geodf)[2] <- "TEAM"
-  write.csv(geodf, "geodata.csv", row.names=FALSE)
+
   
-  college_geo <- read.csv("geodata.csv")
-  bb_data <- read.csv("cbb.csv")
-  
-  library(dplyr)
-  merged_data <- left_join(bb_data, college_geo, by = "TEAM")
-  
-  
-  library(ggplot2)
-  library(tidyverse)
-  merged_data %>%
-    group_by(CONF) %>%
-    summarize(AvgFTR = mean(FTR)) %>%
-    arrange(AvgFTR) %>%
-    mutate(CONF = factor(CONF, levels = CONF, ordered = TRUE)) %>%
-    ggplot(aes(CONF, AvgFTR)) + geom_bar(stat = 'identity') + theme(axis.text.x = element_text(angle = 60, hjust = 1))
-}
-  
+library(ggplot2)
+library(tidyverse)
+
+#Loading data frames
 geodf <- read.csv("geodata.csv")
-colnames(geodf)[2] <- "TEAM"
-write.csv(geodf, "geodata.csv", row.names=FALSE)
 
+# Recoding Geodf to have column name 2 as TEAM 
+colnames(geodf)[2] <- "TEAM"
 college_geo <- read.csv("geodata.csv")
 bb_data <- read.csv("cbb.csv")
 
 library(dplyr)
 merged_data <- left_join(bb_data, college_geo, by = "TEAM")
+
+
+
+#   merged_data %>%
+#     group_by(CONF) %>%
+#     summarize(AvgFTR = mean(FTR)) %>%
+#     arrange(AvgFTR) %>%
+#     mutate(CONF = factor(CONF, levels = CONF, ordered = TRUE)) %>%
+#     ggplot(aes(CONF, AvgFTR)) + geom_bar(stat = 'identity') + theme(axis.text.x = element_text(angle = 60, hjust = 1))
+# 
+#   
+}
+  
