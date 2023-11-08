79b4601481f414d68192eb3373638d92ca24be87
Katelyn Gamble <gamblek24@mail.wlu.edu>
2023-11-08 15:10:04 -0500

    trying this again

--- server.r
+++ server.r
@@ -11,7 +11,5 @@ geodf <- read.csv("geodata.csv")
 colnames(geodf)[2] <- "TEAM"
 college_geo <- read.csv("geodata.csv")
 bb_data <- read.csv("cbb.csv")
-
-library(dplyr)
 merged_data <- left_join(bb_data, college_geo, by = "TEAM")
 
