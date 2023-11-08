78964e19d24c6b0fff6bfa704e626cf5082d76d2
Katelyn Gamble <gamblek24@mail.wlu.edu>
2023-11-08 15:51:05 -0500

    adding little stuff

--- ui.r
+++ ui.r
@@ -4,58 +4,23 @@ library(leaflet)
 library(shinydashboard)
 
 dashboardPage(
-<<<<<<< HEAD
   skin = "purple",
-dashboardHeader(title = "NCAA Basketball", titleWidth = 300),
-dashboardSidebar(
-  sidebarMenu(
-    menuItem("Introduction", tabName = "Introduction"),
-    menuItem("Conferences", tabName = "Conferences")
-  )
-),
-dashboardBody(
- 
-# Introduction Page 
-  tabItems(
-  tabItem("Introduction", tabName = "Introduction"),
-           h2("Welcome!"),
-          box(background = "blue", p(" Here is the data!"))
-           )
-   ),
-  
-# Conference Page 
-  tabItem("Conferences", tabName = "Conferences"),
-           h2("Conferences"),
-           )
-#Conferences tab 
-tabItem(tabName = "Conferences",
-fluidPage(box(width = 12, p("This page shows the NCAA statistics separated by conference, Press on all of the tabs to explore different things!")
-             )), 
-
-
-
-  
-  
-)
- 
-  
-  
-
-
-
-=======
   dashboardHeader(title = "NCAA Basketball"),
   dashboardSidebar(
     sidebarMenu(
       menuItem("Home", tabName = "page1"),
-      menuItem("Success by Conference", tabName = "page2")
+      menuItem("Conference Statistics", tabName = "page2")
     )
   ),
   dashboardBody(
     tabItems(
-      tabItem(tabName = "page1", p("Free Throws")),
-      tabItem(tabName = "page2", p("Percent Win"))
+      tabItem(tabName = "page1", p("Home"),
+      h2("Welcome!"),
+      box(background = "purple", p("Introduction!"))
+      
+      ),
+      tabItem(tabName = "page2", p("Conference Statistics"))
     )
   )
 )
->>>>>>> 8ad66174ebb276017537e7af70014cf9f17e8f9e
+
