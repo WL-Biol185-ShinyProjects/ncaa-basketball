9fc500972722feaa78b080e5af56657abab19486
Allyssa Utecht <utechta24@mail.wlu.edu>
2023-11-27 16:33:25 -0500

    help

--- server.r
+++ server.r
@@ -1,14 +1,9 @@
 library(shiny)
+library(tidyverse)
 library(leaflet)
 library(ggplot2)
-library(tidyverse)
+library(geojsonio)
 library(dplyr)
-
-<<<<<<< HEAD
-conf_stats <- read.csv("conference_stats.csv")
-
-function(input,output,session){
-=======
 
 geodf <- read.csv("geodata.csv")
 colnames(geodf)[2] <- "TEAM"
@@ -18,22 +13,24 @@ bb_data <- read.csv("cbb.csv")
 conf_stats <- read.csv("conference_stats.csv")
 
 server <- function(input, output, session) {
-  browser()
   print("checkpoint")
-  
->>>>>>> ff81bfaa3be39eaeb321def218e24c57d23a7324
+ 
   output$plot <- renderPlot({
- 
-    ggplot(data=conf_stats, aes_string(x='Conference',
-                                        y=input$y_var)) +
-                                        geom_bar(stat = "identity", width = 0.8) +
-                                        labs(x="Conference", y=input$y_var)
- 
+    
+    ggplot(data = df, aes_string(x = 'Conference',
+                                 y = input$y_var)) +
+      geom_bar(stat = "identity", width = 0.8) +
+      labs(x = "Conference", y = input$y_var)
+    ggplot(conf_stats, aes_string(x='Conference',
+                                  y=input$y_var)) +
+      geom_bar(stat = "identity", width = 0.8) +
+      labs(x="Conference", y=input$y_var)
+    
+    
   })
-
+  
   output$map <- renderLeaflet({
     leaflet() %>%
-<<<<<<< HEAD
       addPolygons(
         fillOpacity = 2.5,
         fillColor = ~pal(avgPR),
@@ -48,18 +45,4 @@ server <- function(input, output, session) {
   })
 }
 
-=======
-        addPolygons(
-          fillOpacity = 2.5,
-          fillColor = ~pal(avgPR),
-          color = "white",
-          dashArray = '3',
-          weight = 0.3,
-          data = geo@data
-        )
-      addTiles() %>%
-        
-        addGeoJSON(geojson_data)
-    })
-  }
->>>>>>> ff81bfaa3be39eaeb321def218e24c57d23a7324
+
