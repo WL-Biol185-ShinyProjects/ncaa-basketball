65b5aa3b62cb13027429d7e24e1bc43b4f6b02ab
Katelyn Gamble <gamblek24@mail.wlu.edu>
2023-11-28 22:11:20 -0500

    Added in the heatmap and fixed our app to add in fluid rows

--- server.r
+++ server.r
@@ -1,17 +1,29 @@
+# loading the necessary libraries
 library(shiny)
 library(tidyverse)
 library(leaflet)
 library(ggplot2)
 library(dplyr)
+library(d3heatmap)
 
+# specifying our conference data used in conference tab
 conf_stats <- read.csv("conference_stats.csv")
- 
+
 function(input,output,session){
-  output$plot <- renderPlot({
-    
-    ggplot(data=conf_stats, aes_string(x='Conference',
+  
+  # Conference tab bar graph
+        output$plot <- renderPlot({
+        ggplot(data=conf_stats, aes_string(x='Conference',
                                        y=input$y_var)) +
-      geom_bar(stat = "identity", width = 0.8) +
-      labs(x="Conference", y=input$y_var)
+              geom_bar(stat = "identity", width = 0.8) +
+              labs(x="Conference", y=input$y_var)
   })
+        
+  # Conference Tab Heat Map
+        heatmap_data <- as.matrix(conf_stats[, -1])
+        row_labels <- conf_heatmapdata$Conference
+        output$heatmap <- renderD3heatmap({
+          d3heatmap(heatmap_data, scale = "column", Rowv = row_labels, dendrogram = "none", colors = "Blues")
+        })
+          
 }
--- ui.r
+++ ui.r
@@ -1,6 +1,8 @@
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
+library(d3heatmap)
+conf_stats <- read.csv("conference_stats.csv")
 
 dashboardPage(
   skin = "purple",
@@ -18,14 +20,39 @@ dashboardPage(
               box(background = "purple", p("Introduction!"))
       ),
       
-      tabItem(tabName = "page2", p("Conference Statistics"),
-              selectInput(
-                "y_var",
-                label = "Conference Data",
-                choices = colnames(conf_stats),
-                selected = "Conference"),
+      tabItem(tabName = "page2", 
+              tabPanel("Conference Statistics",
+  # Bar Graph for Conference           
+              fluidRow(
+                box(
+                  title = "Conference Data",
+                  status = "primary",
+                  width = 6,
+                  selectInput(
+                    "y_var",
+                    label = "Conference Data",
+                    choices = colnames(conf_stats),
+                    selected = "Conference")
+                ),
                 plotOutput("plot")
               )
+              ),
+  
+  # Heat Map for Conference
+            fluidRow(
+             box(
+               title = "Conference Heatmap",
+               status = "primary",
+               solidHeader = TRUE,
+               width = 6,
+              d3heatmapOutput("heatmap")
+    )
+  )
+  
+  
+  
+  
       )
     )
   )
+)
